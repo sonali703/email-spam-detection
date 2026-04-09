750cdd1bcef495ad051a67c5122cfedff3700138
sonali703 <2310030025@klh.edu.in>
2026-04-09 13:52:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,5 +28,6 @@ def predict():
     return render_template('index.html', prediction=output)
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
